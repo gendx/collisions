02da38041136a21c424288b37a76818f815b0692
G. Endignoux <ggendx@gmail.com>
2015-12-04 17:13:05 +0100

    Fix clang installation.

--- install/install-clang.sh
+++ install/install-clang.sh
@@ -4,5 +4,5 @@ apt-add-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
 apt-get update -qq
 apt-get install -qq clang-3.5
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/clang++-3.5 90
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/clang++-3.5 91
 
