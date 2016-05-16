7176652a555d4b9dd462db1140ef0054f8170322
G. Endignoux <ggendx@gmail.com>
2016-05-16 14:44:13 +0200

    WIP

--- install/install-clang.sh
+++ install/install-clang.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-add-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
-apt-get update -qq
-apt-get install -qq clang-3.5
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/clang++-3.5 91
 
